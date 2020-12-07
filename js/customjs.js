cd4866e73c748e8a90e64aeb1f1deb485fa8a55e
Alex <alex@growful.co.uk>
2020-12-07 00:22:10 +0000

    made some changes and swapped some content around

--- js/customjs.js
+++ js/customjs.js
@@ -12,7 +12,7 @@ function gfddOnMouseOver(x) {
 }
 
 function gfddOnClick(pageName) {
-  window.location.href = '/' + pageName + '.html'
+  window.location.href = '/' + pageName + '.html#top'
 }
 
 window.onclick = function(event) {
@@ -26,4 +26,19 @@ window.onclick = function(event) {
       }
     }
   }
+}
+
+var cycleIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("gfcycle");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  cycleIndex++;
+  if (cycleIndex > x.length) {cycleIndex = 1}
+  x[cycleIndex-1].style.display = "block";
+  setTimeout(carousel, 2000);
 }
